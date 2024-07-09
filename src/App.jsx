08add9cc244547747995e779c3b815b520406009
Hasan Yahya <hasna786yahya@gmail.com>
2024-07-09 06:51:15 +0500

    refactor: Improve responsiveness for mobile devices in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import CanvasDrawingApp from './components/component/playground';
+import Apology from './components/component/apology';
+
+const isDesktop = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    const isMobile = /mobile|android|iphone|ipad|tablet|touch|samsung|fridge/i.test(userAgent);
+    const isSmallScreen = window.innerWidth <= 1024;
+    return !isMobile && !isSmallScreen;
+};
 
 function App() {
-    return (
-        <CanvasDrawingApp />
-    )
+    if (!isDesktop()) {
+        return <Apology />
+    }
+    else {
+        return (
+            <CanvasDrawingApp />
+        )
+    }
 }
 
 export default App
